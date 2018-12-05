fbed3bd8f0f71835d923b0898b84a25853c29ebf
Thomas Mellenthin <github@dysternis.de>
2018-12-05 08:34:29 +0100

    Started adding commander for command line parsing, reordered OIDs: OID should be the key always

--- rawbone.js
+++ rawbone.js
@@ -10,208 +10,229 @@
  */
 
 // npm install request-digest semaphore
+'use strict';
+
+
+var program = require('commander');
+program
+  .version('0.1.0')
+  .option('-o, --openhab [URL] [OID]', 'Posts the value of the given OID to the openHAB URL.')
+  .option('-l, --list', 'List all known OIDs')
+  .option('-f, --fetch [OIDs]', 'Fetches values for the given OIDs. OIDs must be submitted comma separated without spaces.')
+
+program.on('--help', function(){
+  console.log('')
+  console.log('Examples:');
+  console.log('')
+  console.log('  Fetch the value of 1/60/0/155/1 and post the value to openHAB running at 192.168.23.42:');
+  console.log('')
+  console.log('  $ rawbone -o http://192.168.23.42:8080/foo/bar/X "1/60/0/155/1"');
+  console.log('')
+  console.log('  Fetch the given OIDs and print the values and a description of each value on the console');
+  console.log('')
+  console.log('  $ rawbone -f 1/60/0/155/1,1/65/0/155/1,1/65/0/156/6');
+});
+program.parse(process.argv);
 
 var config = require('./config');
 var PATH="/api/1.0/lookup/";
-
-'use strict';
 
 const OIDs = {
   // 60 BioWIN II
     // 0 BioWIN-II
       // 155 Titelbild
-                // 'Kesseltemperatur Istwert                    ': "1/60/0/155/0",
-                /**
-                 * Betriebsphasen 1/60/0/155/1
-                 *
-                 * 0: Brenner gesperrt
-                 * 1: Selbsttest
-                 * 2: WE ausschalten
-                 * 3: Standby
-                 * 4: Brenner AUS
-                 * 5: Vorspülen
-                 * 6: Zündphase
-                 * 7: Flammenstabilisierung
-                 * 8: Modulationsbetrieb
-                 * 17: Ausbrand
-                 */
-                'Betriebsphasen BioWIN-II                       ': "1/60/0/155/1",
-                // 'Handbetrieb Solltemperatur                  ': "1/60/0/155/2",
-               /**
-                * Betriebsarten 1/60/0/155/3
-                *
-                * 0: Ausgeschaltet
-                * 1: Abschaltvorgang
-                * 2: Festbrennstoff-/ Pufferbetrieb
-                * 3: Pelletszuführung in Betrieb
-                * 4: Pelletszuführung
-                * 5: Ein
-                * 6: Pelletszuführung in Betrieb
-                * 7: Pelletszuführung
-                * 8: Handbetrieb
-                * 9: Kaminkehrfunktion
-                * 10: Aktorentest
-                * 11: Installationsvorgang aktiv
-                */
-                // 'Betriebsarten BioWIN-II                        ': "1/60/0/155/3",
-                // 'Alarmcode                                   ': "1/60/0/155/4",
-                // 'Kaminkehrerbetrieb Leistung                 ': "1/60/0/155/5",
-                // 'Kaminkehrerbetrieb Restlaufzeit             ': "1/60/0/155/6",
-      // 156 Infoebene
-                // 'Solltemperatur                              ': "1/60/0/156/0",
-                // 'Kesseltype                                  ': "1/60/0/156/1",
-                // 'Betriebsstunden BioWIN-II                   ': "1/60/0/156/2",
-                // 'Laufzeit bis Reinigung                      ': "1/60/0/156/3",
-                // 'Weichen-/Puffertemperatur                   ': "1/60/0/156/4",
-                // 'Laufzeit bis Hauptreinigung                 ': "1/60/0/156/5",
-                // 'Laufzeit bis Wartung                        ': "1/60/0/156/6",
-                // 'Aktuelle Kesselleistung                     ': "1/60/0/156/7",
-                // 'Anzahl der Brennerstarts                    ': "1/60/0/156/8",
-                // 'Temperatur Abgas BioWIN-II                  ': "1/60/0/156/9",
-                // 'Pelletverbrauch gesamt                      ': "1/60/0/156/10",
-                // 'Pelletverbrauch seit Befüllung              ': "1/60/0/156/11",
-      // 157 Betreiberebene
-                // /**
-                //  * 0: ausgeschaltet
-                //  * 1: ohne Zeitsteuerung
-                //  * 2: mit Freigabezeit
-                //  * 3: mit Startzeit
-                //  */
-                // 'Betriebsart Zuführung            ': "1/60/0/157/0",
-                // /**
-                //  * 0: alle Sonden
-                //  * 1: nur Sonde 1
-                //  * 2: nur Sonde 2
-                //  * 3: nur Sonde 3
-                //  * 4: nur Sonde 4
-                //  * 5: nur Sonde 5
-                //  * 6: nur Sonde 6
-                //  * 7: nur Sonde 7
-                //  * 8: nur Sonde 8
-                //  * 16: nur Zone 1
-                //  * 17: nur Zone 2
-                //  */
-                // 'Sondenumschaltung                           ': "1/60/0/157/1",
-                // 'Zuführung mit Freigabezeit Ende             ': "1/60/0/157/2",
-                // 'Zuführung mit Freigabezeit Begin            ': "1/60/0/157/3",
-                // 'Zuführung mit Startzeit Startzeit           ': "1/60/0/157/4",
-                // 'Startzeiten für Ascheverdichtung Startzeit 1': "1/60/0/157/5",
-                // 'Startzeiten für Ascheverdichtung Startzeit 2': "1/60/0/157/6",
-                // 'Heizflächenreinigung Beginn Sperrzeit       ': "1/60/0/157/7",
-                // 'Uhrzeit                                     ': "1/60/0/157/8",
-                // 'Datum                                       ': "1/60/0/157/8",
-      // 158 Serviceebene
-                /**
-                 * Betriebswahl
-                 * 0: AUS
-                 * 1: EIN
-                 * 2: Handbetrieb
-                 * 3: Kaminkehrbetrieb
-                 * 4: Aktorentest
-                 */
-                // 'Betriebswahl                                ': "1/60/0/158/0",
-                // 'Hysterese Brenner EIN                       ': "1/60/0/158/1",
-                // 'Maximalwert der Solltemperatur              ': "1/60/0/158/2",
-                // 'Solltemperatur ext. Wärmeanforderung        ': "1/60/0/158/3",
-                // 'Anzahl der Rostrüttelungen Ausbrand         ': "1/60/0/158/4",
-                // 'Anzahl der Rostrüttelungen Betrieb          ': "1/60/0/158/5",
-                // 'Laufzeit der Saugturbine                    ': "1/60/0/158/6",
-                // 'Zuluftklappe Laufzeit                       ': "1/60/0/158/7",
-                // 'Förderzeit Zündphase                        ': "1/60/0/158/8",
-                // 'Grenzen für Gebläsedrehzahl Minimalwert     ': "1/60/0/158/9",
-  // 65 LogWIN
-    // 0 LogWIN
-      // 155 Titelbild
-                /**
-                 * 0: Ausgeschaltet
-                 * 1: Abschaltvorgang
-                 * 2: Festbrennstoff-/ Pufferbetrieb
-                 * 3: Pelletszuführung in Betrieb
-                 * 4: Pelletszuführung
-                 * 5: Ein
-                 * 6: Pelletszuführung in Betrieb
-                 * 7: Pelletszuführung
-                 * 8: Handbetrieb
-                 * 9: Kaminkehrerfunktion
-                 * 10: Aktorentest
-                 * 11: Installationsvorgang aktiv
-                 */
-                // 'Betriebsarten LogWIN                        ': "1/65/0/155/0",
-                /**
-                 * 1: Selbsttest
-                 * 2: WE ausschalten
-                 * 3: Standby
-                 * 6: Zündphase
-                 * 7: Flammenstabilisierung
-                 * 8: Modulationsbetrieb
-                 * 9: Kessel gesperrt
-                 * 10: Standby Sperrzeit
-                 * 11: Gebläse AUS
-                 * 12: Verkleidungstür offen
-                 * 13: Zündung bereit
-                 * 14: Abbruch Zündphase
-                 * 15: Anheizvorgang
-                 * 16: Schichtladung
-                 * 17: Ausbrand
-                 */
-                'Betriebsphasen LogWIN                        ': "1/65/0/155/1",
-                // 'Kesseltemperatur Istwert                    ': "1/65/0/155/2",
-                // 'Alarmcode                                   ': "1/65/0/155/3",
-      // 156 Titelbild
-                // 'Kesseltype                                  ': "1/65/0/156/0",
-                // 'Betriebsstunden LogWIN                      ': "1/65/0/156/1",
-                // 'Puffertemperatur Oben                       ': "1/65/0/156/2",
-                // 'Puffertemperatur Unten                      ': "1/65/0/156/3",
-                // 'Puffertemperatur Mitte                      ': "1/65/0/156/4",
-                // 'Anzahl Anheizvorgänge                       ': "1/65/0/156/5",
-                // 'Temperatur Abgas LogWIN                     ': "1/65/0/156/6",
-                // 'Brennkammertemperatur                       ': "1/65/0/156/7",
-                // 'Puffer-Beladegrad                           ': "1/65/0/156/8",
-                // 'Softwareversion Feuerungsautomat            ': "1/65/0/156/9",
-      // 157 Betreiberebene
-                // 'Startverzögerung Automatikkessel            ': "1/65/0/157/0",
-                // 'Uhrzeit                                     ': "1/65/0/157/1",
-                // 'Datum                                       ': "1/65/0/157/2",
-                // 'Sperrzeit bis                               ': "1/65/0/157/3",
-                // 'Startverzögerung Abbruch                    ': "1/65/0/157/4",
-      // 158 Serviceebene
-                /**
-                 * 0: AUS
-                 * 1: EIN
-                 * 2: Handbetrieb
-                 * 3: Kaminkehrerbetrieb
-                 * 4: Aktorentest
-                 * 5:
-                 */
-                // 'Betriebswahl                                ': "1/65/0/158/0",
-                // 'Kesseltemperatur für Neustart               ': "1/65/0/158/1",
-                // 'Saugzuggebläse Soll-Drehzahl                ': "1/65/0/158/2",
-                // 'Position Primär-LK                          ': "1/65/0/158/3",
-                // 'Position Sekundär-LK                        ': "1/65/0/158/4",
-                // 'O2 Signal                                   ': "1/65/0/158/5",
-                /**
-                 * 0: Nein
-                 * 1: Ja
-                 */
-                // 'Elektrische Zü˜ndung                         ': "1/65/0/158/6",
-                /**
-                 * 0: Automatische Erkennung
-                 * 1: Normal
-                 * 2: Gluterhaltung
-                 */
-                // 'Ausbrand                                    ': "1/65/0/158/7",
-                // 'O2 Heizstrom                                ': "1/65/0/158/8",
-                /**
-                 * 0: Nein
-                 * 1: Ja
-                 */
-                // 'Kombikessel                                 ': "1/65/0/158/9",
-              };
+      '1/60/0/155/0': 'Kesseltemperatur Istwert                    ',
+      /**
+       * Betriebsphasen 1/60/0/155/1
+       *
+       * 0: Brenner gesperrt
+       * 1: Selbsttest
+       * 2: WE ausschalten
+       * 3: Standby
+       * 4: Brenner AUS
+       * 5: Vorspülen
+       * 6: Zündphase
+       * 7: Flammenstabilisierung
+       * 8: Modulationsbetrieb
+       * 17: Ausbrand
+       */
+      '1/60/0/155/1': 'Betriebsphasen BioWIN-II                    ',
+      '1/60/0/155/2': 'Handbetrieb Solltemperatur                  ',
+      /**
+       * Betriebsarten 1/60/0/155/3
+       *
+       * 0: Ausgeschaltet
+       * 1: Abschaltvorgang
+       * 2: Festbrennstoff-/ Pufferbetrieb
+       * 3: Pelletszuführung in Betrieb
+       * 4: Pelletszuführung
+       * 5: Ein
+       * 6: Pelletszuführung in Betrieb
+       * 7: Pelletszuführung
+       * 8: Handbetrieb
+       * 9: Kaminkehrfunktion
+       * 10: Aktorentest
+       * 11: Installationsvorgang aktiv
+       */
+      '1/60/0/155/3': 'Betriebsarten BioWIN-II                     ',
+      '1/60/0/155/4': 'Alarmcode                                   ',
+      '1/60/0/155/5': 'Kaminkehrerbetrieb Leistung                 ',
+      '1/60/0/155/6': 'Kaminkehrerbetrieb Restlaufzeit             ',
+    // 156 Infoebene
+      '1/60/0/156/0': 'Solltemperatur                              ',
+      '1/60/0/156/1': 'Kesseltype                                  ',
+      '1/60/0/156/2': 'Betriebsstunden BioWIN-II                   ',
+      '1/60/0/156/3': 'Laufzeit bis Reinigung                      ',
+      '1/60/0/156/4': 'Weichen-/Puffertemperatur                   ',
+      '1/60/0/156/5': 'Laufzeit bis Hauptreinigung                 ',
+      '1/60/0/156/6': 'Laufzeit bis Wartung                        ',
+      '1/60/0/156/7': 'Aktuelle Kesselleistung                     ',
+      '1/60/0/156/8': 'Anzahl der Brennerstarts                    ',
+      '1/60/0/156/9': 'Temperatur Abgas BioWIN-II                  ',
+      '1/60/0/156/10': 'Pelletverbrauch gesamt                     ',
+      '1/60/0/156/11': 'Pelletverbrauch seit Befüllung             ',
+    // 157 Betreiberebene
+      /**
+       * 0: ausgeschaltet
+       * 1: ohne Zeitsteuerung
+       * 2: mit Freigabezeit
+       * 3: mit Startzeit
+       */
+      '1/60/0/157/0': 'Betriebsart Zuführung                       ',
+      /**
+       * 0: alle Sonden
+       * 1: nur Sonde 1
+       * 2: nur Sonde 2
+       * 3: nur Sonde 3
+       * 4: nur Sonde 4
+       * 5: nur Sonde 5
+       * 6: nur Sonde 6
+       * 7: nur Sonde 7
+       * 8: nur Sonde 8
+       * 16: nur Zone 1
+       * 17: nur Zone 2
+       */
+      '1/60/0/157/1': 'Sondenumschaltung                           ',
+      '1/60/0/157/2': 'Zuführung mit Freigabezeit Ende             ',
+      '1/60/0/157/3': 'Zuführung mit Freigabezeit Begin            ',
+      '1/60/0/157/4': 'Zuführung mit Startzeit Startzeit           ',
+      '1/60/0/157/5': 'Startzeiten für Ascheverdichtung Startzeit 1',
+      '1/60/0/157/6': 'Startzeiten für Ascheverdichtung Startzeit 2',
+      '1/60/0/157/7': 'Heizflächenreinigung Beginn Sperrzeit       ',
+      '1/60/0/157/8': 'Uhrzeit                                     ',
+      '1/60/0/157/8': 'Datum                                       ',
+    // 158 Serviceebene
+      /**
+       * Betriebswahl
+       * 0: AUS
+       * 1: EIN
+       * 2: Handbetrieb
+       * 3: Kaminkehrbetrieb
+       * 4: Aktorentest
+       */
+      '1/60/0/158/0': 'Betriebswahl                                ',
+      '1/60/0/158/1': 'Hysterese Brenner EIN                       ',
+      '1/60/0/158/2': 'Maximalwert der Solltemperatur              ',
+      '1/60/0/158/3': 'Solltemperatur ext. Wärmeanforderung        ',
+      '1/60/0/158/4': 'Anzahl der Rostrüttelungen Ausbrand         ',
+      '1/60/0/158/5': 'Anzahl der Rostrüttelungen Betrieb          ',
+      '1/60/0/158/6': 'Laufzeit der Saugturbine                    ',
+      '1/60/0/158/7': 'Zuluftklappe Laufzeit                       ',
+      '1/60/0/158/8': 'Förderzeit Zündphase                        ',
+      '1/60/0/158/9': 'Grenzen für Gebläsedrehzahl Minimalwert     ',
+// 65 LogWIN
+  // 0 LogWIN
+    // 155 Titelbild
+      /**
+       * 0: Ausgeschaltet
+       * 1: Abschaltvorgang
+       * 2: Festbrennstoff-/ Pufferbetrieb
+       * 3: Pelletszuführung in Betrieb
+       * 4: Pelletszuführung
+       * 5: Ein
+       * 6: Pelletszuführung in Betrieb
+       * 7: Pelletszuführung
+       * 8: Handbetrieb
+       * 9: Kaminkehrerfunktion
+       * 10: Aktorentest
+       * 11: Installationsvorgang aktiv
+       */
+      '1/65/0/155/0': 'Betriebsarten LogWIN                        ',
+      /**
+       * 1: Selbsttest
+       * 2: WE ausschalten
+       * 3: Standby
+       * 6: Zündphase
+       * 7: Flammenstabilisierung
+       * 8: Modulationsbetrieb
+       * 9: Kessel gesperrt
+       * 10: Standby Sperrzeit
+       * 11: Gebläse AUS
+       * 12: Verkleidungstür offen
+       * 13: Zündung bereit
+       * 14: Abbruch Zündphase
+       * 15: Anheizvorgang
+       * 16: Schichtladung
+       * 17: Ausbrand
+       */
+      '1/65/0/155/1': 'Betriebsphasen LogWIN                       ',
+      '1/65/0/155/2': 'Kesseltemperatur Istwert                    ',
+      '1/65/0/155/3': 'Alarmcode                                   ',
+    // 156 Titelbild
+      '1/65/0/156/0': 'Kesseltype                                  ',
+      '1/65/0/156/1': 'Betriebsstunden LogWIN                      ',
+      '1/65/0/156/2': 'Puffertemperatur Oben                       ',
+      '1/65/0/156/3': 'Puffertemperatur Unten                      ',
+      '1/65/0/156/4': 'Puffertemperatur Mitte                      ',
+      '1/65/0/156/5': 'Anzahl Anheizvorgänge                       ',
+      '1/65/0/156/6': 'Temperatur Abgas LogWIN                     ',
+      '1/65/0/156/7': 'Brennkammertemperatur                       ',
+      '1/65/0/156/8': 'Puffer-Beladegrad                           ',
+      '1/65/0/156/9': 'Softwareversion Feuerungsautomat            ',
+    // 157 Betreiberebene
+      '1/65/0/157/0': 'Startverzögerung Automatikkessel            ',
+      '1/65/0/157/1': 'Uhrzeit                                     ',
+      '1/65/0/157/2': 'Datum                                       ',
+      '1/65/0/157/3': 'Sperrzeit bis                               ',
+      '1/65/0/157/4': 'Startverzögerung Abbruch                    ',
+    // 158 Serviceebene
+      /**
+       * 0: AUS
+       * 1: EIN
+       * 2: Handbetrieb
+       * 3: Kaminkehrerbetrieb
+       * 4: Aktorentest
+       * 5:
+       */
+      '1/65/0/158/0': 'Betriebswahl                                ',
+      '1/65/0/158/1': 'Kesseltemperatur für Neustart               ',
+      '1/65/0/158/2': 'Saugzuggebläse Soll-Drehzahl                ',
+      '1/65/0/158/3': 'Position Primär-LK                          ',
+      '1/65/0/158/4': 'Position Sekundär-LK                        ',
+      '1/65/0/158/5': 'O2 Signal                                   ',
+      /**
+       * 0: Nein
+       * 1: Ja
+       */
+      '1/65/0/158/6': 'Elektrische Zündung                         ',
+      /**
+       * 0: Automatische Erkennung
+       * 1: Normal
+       * 2: Gluterhaltung
+       */
+      '1/65/0/158/7': 'Ausbrand                                    ',
+      '1/65/0/158/8': 'O2 Heizstrom                                ',
+      /**
+       * 0: Nein
+       * 1: Ja
+       */
+      '1/65/0/158/9': 'Kombikessel                                 ',
+    };
 
 var sem = require('semaphore')(1);
 
 var digestRequest = require('request-digest')(config.USERNAME, config.PASSWORD);
-for (const [description, oid] of Object.entries(OIDs)) {
+for (const [oid, description] of Object.entries(OIDs)) {
   sem.take(function() {
     digestRequest.requestAsync({
       host: config.HOST,
